6d611d120a7cb2a9132124056bde61f4f12186a1
Michael Barton <mail@michaelbarton.me.uk>
2017-04-12 14:08:15 -0700

    Change path gaet is installed to

--- image/bin/install/gaet.sh
+++ image/bin/install/gaet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 fetch.sh https://s3-us-west-1.amazonaws.com/gaet/releases/${GAET_VERSION}.tar.xz gaet-tmp
-mv /usr/local/gaet-tmp/gaet-${GAET_VERSION}/ /usr/local/geat/
-ln -s /usr/local/geat/bin/* /usr/local/bin
+mv /usr/local/gaet-tmp/binary/gaet-${GAET_VERSION}/ /usr/local/gaet/
+ln -s /usr/local/gaet/bin/* /usr/local/bin
